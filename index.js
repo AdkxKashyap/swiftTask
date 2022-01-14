8974ec4e8b254648d60f068d78a88a6ab7e50193
Akashdeep Kashyap <akashdeep.kashyap@macys.com>
2022-01-15 01:11:15 +0530

    pre deploy3

--- index.js
+++ index.js
@@ -17,18 +17,6 @@ app.use(express.json())//parses incoming requests to json object
 app.use(userRoutes)//to make a file and small we have moved the routes to a different folder
 app.use(tasksRoutes)
 
-
-// app.post('/tasks',async(req,res)=>{//USING router to keep this file simple
-//     const task=new tasks(req.body)
-//     try {
-//         await task.save()
-//         res.status(201).send(task)
-//     } catch (error) {
-//         res.status(500).send(error)
-//     }
-// })
-
-
 app.listen(port,()=>{
     console.log("Server is running on port"+port)
 })
